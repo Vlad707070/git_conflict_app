f3145dbdce8966531ff717b53ee547234770a468
Владислав Самоделов <gr643_slmma@lab-203-2.tomtit-tomsk.ru>
2026-04-16 17:52:30 +0700

    Add multiplication function

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 def main():
     print("=== Калькулятор ===")
     print("1. Сложение")
     print("2. Вычитание")
+    print("3. Умножение")
     choice = input("Выберите операцию: ")
     
     if choice == "1":
@@ -18,6 +22,10 @@ def main():
         x = float(input("Число 1: "))
         y = float(input("Число 2: "))
         print(f"Результат: {sub(x, y)}")
+    elif choice == "3":
+        x = float(input("Число 1: "))
+        y = float(input("Число 2: "))
+        print(f"Результат: {mul(x, y)}")
 
 if __name__ == "__main__":
     main()
