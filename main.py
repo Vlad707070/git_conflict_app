843e4ae565e82705e19b378a37039775b8eb5717
Владислав Самоделов <gr643_slmma@lab-203-2.tomtit-tomsk.ru>
2026-04-16 17:50:39 +0700

    Add subtraction function

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def main():
     print("=== Калькулятор ===")
     print("1. Сложение")
+    print("2. Вычитание")
     choice = input("Выберите операцию: ")
     
     if choice == "1":
         x = float(input("Число 1: "))
         y = float(input("Число 2: "))
         print(f"Результат: {add(x, y)}")
+    elif choice == "2":
+        x = float(input("Число 1: "))
+        y = float(input("Число 2: "))
+        print(f"Результат: {sub(x, y)}")
 
 if __name__ == "__main__":
     main()
